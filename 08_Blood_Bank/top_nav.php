82919969a37aedb76e33a7b375f685837391dfda
ashikdayanand6 <122517252+ashikdayanand6@users.noreply.github.com>
2024-03-28 12:44:07 +0530

    Files

---
+++ 08_Blood_Bank/top_nav.php
@@ -0,0 +1,29 @@
+    <!-- Navigation -->
+    <div class=" container">
+    <nav class="navbar" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php" style="font-size:30px;margin-top:-12%"><i class="fa fa-heartbeat fa-lg"></i> Blood Source</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right" style="margin-top:-2%;">
+                    <li><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+                    <li><a href="about.php"><i class="fa fa-users"></i> About us</a></li>
+                    <li><a href="Donor_reg.php"><i class="fa fa-briefcase"></i> Services</a></li>
+                    <li><a href="contact.php"><i class="fa fa-envelope"></i> Contact us</a></li>
+                    <li><a href="admin.php"><i class="fa fa-user-md"></i> Admin</a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+</div>
